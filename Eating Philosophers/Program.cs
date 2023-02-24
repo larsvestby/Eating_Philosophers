c87b9810e3f42f9de55d889ca97f0b462b978aeb
Lars Vestby <97159154+larsvestby@users.noreply.github.com>
2023-02-24 09:06:54 +0100

    Add files via upload

--- Eating Philosophers/Program.cs
+++ Eating Philosophers/Program.cs
@@ -1,28 +1,26 @@
 ﻿using Eating_Philosophers;
 
-/*
-En filosof kan have tre stadier:
+/*En filosof kan have tre stadier:
 
 Grøn = De spiser!
 Gul = De tænker!
 Rød = De sulter!
 
-Extra idé, gør sådan at de kan sulte ihjel.
- */
+Extra idé, gør sådan at de kan sulte ihjel.*/
 Random random= new();
+Philosophers[] philosophers = new Philosophers[5];
 
 //Her instantieres filosofferne. -Bemærk forks. De er forskudt for at simulere et rundt bord.
-Philosophers kennyM = new(Table.Fork5, Table.Fork1, "Kenny McCormick", random.Next(3,10));
-Philosophers ericC = new(Table.Fork1, Table.Fork2, "Eric Cartmann", random.Next(3, 10));
-Philosophers kyleB = new(Table.Fork2, Table.Fork3, "Kyle Broflovski", random.Next(3, 10));
-Philosophers stanM = new(Table.Fork3, Table.Fork4, "Stan Marsh", random.Next(3, 10));
-Philosophers randyM = new(Table.Fork4, Table.Fork5, "Randy Marsh", random.Next(3, 10));
+philosophers[0] = new(Table.Fork5, Table.Fork1, "Kenny McCormick", random.Next(3, 10));
+philosophers[1] = new(Table.Fork1, Table.Fork2, "Eric Cartmann", random.Next(3, 10));
+philosophers[2] = new(Table.Fork2, Table.Fork3, "Kyle Broflovski", random.Next(3, 10));
+philosophers[3] = new(Table.Fork3, Table.Fork4, "Stan Marsh", random.Next(3, 10));
+philosophers[4] = new(Table.Fork4, Table.Fork5, "Randy Marsh", random.Next(3, 10));
 
 //Her startes en ny tråd for hver filosof. -Bemærk at hver filosof starter med at tænke.
-new Thread(kennyM.Think).Start();
-new Thread(ericC.Think).Start();
-new Thread(kyleB.Think).Start();
-new Thread(stanM.Think).Start();
-new Thread(randyM.Think).Start();
+foreach (var item in philosophers)
+{
+    new Thread(item.Think).Start();
+}
 
 Console.ReadKey();
